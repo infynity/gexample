cd644c7f9ae86262ff1f8d146532b8ac846901d4
infy <1278082436@qq.com>
2020-10-10 09:56:05 +0800

    ioc

---
+++ ioc/services/AdminService.go
@@ -0,0 +1,8 @@
+package services
+type AdminService struct {
+	Order *OrderService `inject:"ServiceConfig.OrderService()"`
+}
+
+func NewAdminService() *AdminService {
+	return &AdminService{}
+}
---
+++ ioc/services/UserService.go
@@ -0,0 +1,19 @@
+package services
+
+import "fmt"
+
+type UserService struct {
+	Order *OrderService `inject:"ServiceConfig.OrderService()"`
+}
+
+func NewUserService() *UserService {
+	return &UserService{}
+}
+
+ 
+func(this *UserService) GetUserInfo(uid int){
+	fmt.Println("获取用户ID=",uid,"的详细信息")
+}
+func(this *UserService) GetOrderInfo(uid int ){
+	this.Order.GetOrderInfo(uid)
+}
