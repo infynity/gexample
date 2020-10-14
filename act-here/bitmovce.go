d8e31922738c794aefe6b8f5f9c64e90e54d726e
infy <1278082436@qq.com>
2020-10-14 16:29:34 +0800

    fooo

--- act-here/bitmovce.go
+++ act-here/bitmovce.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 func main2(){
 	fmt.Println(9>>1)
@@ -9,7 +13,7 @@ func main2(){
 	// 1001   10010
 }
 //-------------------------------------------------------------------------
-func main() {
+func main3() {
 	i := 10 //整形变量 i
 	ip := &i //指向整型变量 i 的指针ip,包含了 i 的内存地址
 	fmt.Printf("main中i的值为：%v，i 的内存地址为：%v，i的指针的内存地址为：%v\n",i,ip,&ip)
@@ -23,3 +27,32 @@ func modifyBypointer(i *int) {
 }
 
 //-------------------------------------------------------------------------
+
+func maina(){
+	arr := [5]int{1,2,3,4,5}
+
+
+	fmt.Printf("%p\n",&arr)
+	slc := arr[:]
+	fmt.Println(&slc[0])
+	fmt.Printf("%p\n",&slc)
+
+	slc[2]=100
+
+	fmt.Println(slc,arr)
+}
+
+
+//-------------------------------------------------------------------------
+
+func main() {
+
+	var s1 []int
+	s2 := make([]int,0)
+	s4 := make([]int,0)
+
+	fmt.Printf("s1 pointer:%+v, s2 pointer:%+v, s4 pointer:%+v, \n", *(*reflect.SliceHeader)(unsafe.Pointer(&s1)),*(*reflect.SliceHeader)(unsafe.Pointer(&s2)),*(*reflect.SliceHeader)(unsafe.Pointer(&s4)))
+	fmt.Printf("%v\n", (*(*reflect.SliceHeader)(unsafe.Pointer(&s1))).Data==(*(*reflect.SliceHeader)(unsafe.Pointer(&s2))).Data)
+	fmt.Printf("%v\n", (*(*reflect.SliceHeader)(unsafe.Pointer(&s2))).Data==(*(*reflect.SliceHeader)(unsafe.Pointer(&s4))).Data)
+}
+
