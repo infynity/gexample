4cd1b374f86b936f75643c68ceaefe26f83ec188
wangfeihu <wangfeihu@uniontech.com>
2021-01-07 13:57:07 +0800

    4'

---
+++ hishenyi/oauth2/4/s.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
+	"goauth/utils"
+	"log"
+	"net/http"
+)
+var srv  *server.Server
+
+func main() {
+
+	manager := manage.NewDefaultManager()
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	clientStore := store.NewClientStore()
+	err:=clientStore.Set("clienta", &models.Client{
+		ID:     "clienta",
+		Secret: "123",
+		Domain: "http://localhost:8080",
+	})
+	if err!=nil{
+		log.Fatal(err)
+	}
+	manager.MapClientStorage(clientStore)
+
+
+	srv = server.NewDefaultServer(manager)
+	srv.SetUserAuthorizationHandler(userAuthorizeHandler)
+
+	r:=gin.New()
+	 r.Use(utils.ErrorHandler())
+	//响应授权码
+	r.GET("/auth", func(context *gin.Context) {
+		err:=srv.HandleAuthorizeRequest(context.Writer,context.Request)
+		if err!=nil{
+			panic(err.Error())
+		}
+	})
+	r.POST("/token", func(context *gin.Context) {
+		err:=srv.HandleTokenRequest(context.Writer,context.Request)
+		if err!=nil{
+			panic(err.Error())
+		}
+	})
+	r.Any("/login", func(c *gin.Context) {
+		data:=map[string]string{
+			"error":"",
+		}
+		if c.Request.Method=="POST"{
+			uname,upass:=c.PostForm("userName"),c.PostForm("userPass")
+			if uname+upass=="shenyi123"{
+				utils.SaveUserSession(c,uname)
+				c.Redirect(302,"/auth?"+c.Request.URL.RawQuery)
+				return
+			}else{
+				data["error"]="用户名密码错误"
+			}
+		}
+		c.HTML(200, "login.html", data)
+	})
+	r.LoadHTMLGlob("public/*.html")
+	r.Run(":80")
+
+}
+func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+		  if userID=utils.GetUserSession(r);userID==""{
+			  w.Header().Set("Location", "/login?"+r.URL.RawQuery)
+			  w.WriteHeader(302)
+		  }
+		return
+}
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
+	err := srv.HandleTokenRequest(w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
