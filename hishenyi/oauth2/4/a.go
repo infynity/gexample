1d6877ee0ba9fb3d1eae4fe40ed14aa2baf2d2f2
wangfeihu <1278082436@qq.com>
2021-01-07 13:57:07 +0800

    4'

--- hishenyi/oauth2/4/a.go
+++ hishenyi/oauth2/4/a.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
-	"net/url"
 )
 const (
 	authServerURL = "http://oauth.jtthink.com"
@@ -22,18 +20,16 @@ var (
 	}
 
 )
+
 func main()  {
+
 	r := gin.Default()
-	 //r.LoadHTMLGlob("public/*")
-
-	codeUrl,_:=url.ParseRequestURI("http://localhost:8080/getcode")
-
-	loginUrl:="http://oauth.jtthink.com/auth?" +
-		"response_type=code&client_id=clienta1&redirect_uri="+
-		codeUrl.String()
-
-	fmt.Println(codeUrl)
-
+	 r.LoadHTMLGlob("public/*")
+	//codeUrl,_:=url.ParseRequestURI("http://localhost:8080/getcode")
+	//loginUrl:="http://oauth.jtthink.com/auth?" +
+	//	"response_type=code&client_id=clienta&redirect_uri="+
+	//	codeUrl.String()
+	loginUrl:=oauth2Config.AuthCodeURL("myclient")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "a-index.html", map[string]string{
@@ -42,9 +38,15 @@ func main()  {
 	})
 	r.GET("/getcode", func(c *gin.Context) {
 		code,_:=c.GetQuery("code")
-		c.JSON(200,gin.H{"code":code})
-	})
-	r.LoadHTMLGlob("public/*")
+	//	 c.JSON(200,gin.H{"code":code})
+		token,err:=oauth2Config.Exchange(c,code)
+		if err != nil {
+			c.JSON(400,gin.H{"message":err.Error()})
+		}else{
+			c.JSON(200,token)
+		}
 
+
+	})
 	r.Run(":8080")
 }
