89bb5923cf7f8fcf1c68213d66e653cbc55d705d
wangfeihu <1278082436@qq.com>
2021-01-29 14:40:54 +0800

    test gld cmt

--- factory/rancher-k8s/src/docker-6.go
+++ factory/rancher-k8s/src/docker-6.go
@@ -9,38 +9,39 @@ import (
 	"github.com/docker/go-connections/nat"
 	"log"
 )
-type Empty struct {}
-func main()  {
+
+type Empty struct{}
+
+func main() {
 	cli, err := client.NewClient("tcp://49.234.65.113:2345", "v1.41", nil, nil)
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-	ctx:=context.Background()
-	config:=&container.Config{
-		WorkingDir:"/app",
+	ctx := context.Background()
+	config := &container.Config{
+		WorkingDir: "/app",
 		ExposedPorts: nat.PortSet{
-			"80/tcp":Empty{},
+			"80/tcp": Empty{},
 		},
-		Image:"alpine:3.12",
-		Cmd:[]string{"./kkk"},
+		Image: "alpine:3.12",
+		Cmd:   []string{"./kkk"},
 	}
-	hostConfig:=&container.HostConfig{
-		PortBindings:nat.PortMap{
-			"80/tcp":[]nat.PortBinding{
-					{HostPort:"80"},//宿主机的端口
+	hostConfig := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"80/tcp": []nat.PortBinding{
+				{HostPort: "80"}, //宿主机的端口
 			},
 		},
-		Binds:[]string{"/root:/app"},
+		Binds: []string{"/root:/app"},
 	}
-	ret,err:=cli.ContainerCreate(ctx,config,hostConfig,nil,nil,"tes2")
-	if err!=nil{
+	ret, err := cli.ContainerCreate(ctx, config, hostConfig, nil, nil, "tes2")
+	if err != nil {
 		log.Fatal(err)
 	}
-	err=cli.ContainerStart(ctx,ret.ID,types.ContainerStartOptions{})
-	if err!=nil{
+	err = cli.ContainerStart(ctx, ret.ID, types.ContainerStartOptions{})
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("容器启动成功,ID是:",ret.ID)
-
+	fmt.Println("容器启动成功,ID是:", ret.ID)
 
 }
